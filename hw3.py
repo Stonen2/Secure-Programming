b15d7513f728a8e93012f304a33572fbbcede54f
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-02-11 21:06:29 -0500

    Add files via upload
    Added Dictionary Reading as well as hashing seperation

--- hw3.py
+++ hw3.py
@@ -2,39 +2,71 @@ import hashlib
 
 
 rawdata = list()
+dictionary = list()
 
-def fun1():
-    mylist = list()
-    with open("crackme.txt") as f:
-        for line in f:
-            mylist(line)
-    return mylist
-def test():
-    print('hello world')
+#def fun1():
+ #   mylist = list()
+ #   with open("crackme.txt") as f:
+  #      for line in f:
+ #           mylist(line)
+ #   return mylist
+#def test():
+   # print('hello world')
 
-    uncrack = list() 
-    userhashes = list()
-    usernames = list() 
-
-
-
-
-    file = open("crackme.txt","r")
-
-    print(file)
+ #   uncrack = list() 
+  #  userhashes = list()
+   # usernames = list() 
 
 
-    print(file.readline())
 
 
-    file.close
+#    file = open("crackme.txt","r")
 
-    uncrack = fun1()
-    print("   \n")
+ #   print(file)
+#
 
-    print(uncrack)
+   # print(file.readline())
 
 
+   # file.close
+
+   # uncrack = fun1()
+  #  print("   \n")
+
+  # print(uncrack)
+
+#Reads in all of the dictionary into memory  
+def getdictionary():
+  #print("Hello World")
+  with open("/usr/share/dict/words","r") as g:
+        text = g.readlines()
+        for line in text:
+          dictionary.append(line)
+
+#Ballroom blitz?         
+def dictionaryAttack():
+  print("Hello World")  
+
+#Prints the given array Nothin special 
+def printarray(list):
+  for i in list: 
+      print(i)
+#Open the Users and Passwords to CRACK 
+def grabTheFile():
+    with open("crackme.txt","r") as g:
+        text = g.readlines()
+        for line in text:
+            temp = line.split(':')
+
+            #print(line)
+            rawdata.append(temp[0])
+            rawdata.append(temp[1])
+            rawdata.append(temp[3])
+            
+            #rawdata.append(line)
+    
+
+#Main Main Main
 def main():
     #f = open("crackme.txt")
     #text = f.readlines()
@@ -44,14 +76,18 @@ def main():
     #    rawdata = f.readline()
     
     #f.close
-
-
-    with open("crackme.txt","r") as g:
-        text = g.readlines()
-        for line in text:
-            print(line)
+    grabTheFile()
+    printarray(rawdata)
+    getdictionary()
+    #print(dictionary[1000])
+    #print(rawdata)
     
-    print(rawdata)
+   # for line4 in rawdata:
+   #     print(line4)
+
+    #m = hashlib.sha256()
+    #m.update("hello world")
+    #m.hexdigest()
 
    
     #for s in rawdata:
