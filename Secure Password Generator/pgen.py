e985c228c62c8d7d422295b249f92bd6ddf235a9
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-05-19 19:45:11 -0400

    Add files via upload
    Establishing Proper Directories

---
+++ Secure Password Generator/pgen.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+def loaddic():
+    print("Working")
+
+
+
+def mutatepass():
+    print("Working")
+
+def genpass():
+    print("Working")
+
+
+
+def main():
+    loaddic()
+    mutatepass()
+    genpass()
+    
+
+    print("Hello Universe")
+
+
+
+
+
+main()
