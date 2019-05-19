5cbcd059accd7c2e5e9842c5678940a3a83d9871
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-05-19 19:52:38 -0400

    Add files via upload

--- Secure Password Generator/pgen.py
+++ Secure Password Generator/pgen.py
@@ -1,11 +1,29 @@
+import random
 
 
 
+
+
+#Generate a 20 Character Password
+#Randomly Pick from the 3 Rules such that rules cannot be guessed
+#First Rule pick a 8,9,10 Character Dictionary Password
+#Mutate the dictionary Word --> Change random Characters to special characters
+#Second Rule Generate Random 10 Numbers
+#Third Rule Salt The Dictionary pass and Mutate Numbers
 
 
 
 def loaddic():
+    print("Hello World")
+
+
+def ruleone():
+    for x in range(10):
+      print(random.randint(1,21)*5)
     print("Working")
+
+def getdictWords():
+    print("Hello World")
 
 
 
@@ -21,7 +39,7 @@ def main():
     loaddic()
     mutatepass()
     genpass()
-    
+
 
     print("Hello Universe")
 
