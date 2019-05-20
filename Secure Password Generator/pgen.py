32e67e03d1b45d5635d4b09127e118857f195f5a
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-05-19 20:29:45 -0400

    Add files via upload
    Rule 2 Implemented

--- Secure Password Generator/pgen.py
+++ Secure Password Generator/pgen.py
@@ -11,7 +11,7 @@ words = list()
 #Second Rule Generate Random 10 Numbers
 #Third Rule Salt The Dictionary pass and Mutate Numbers
 
-
+word = ''
 
 
 
@@ -21,8 +21,6 @@ def ruleone():
         c = random.randint(0,9)
         a = a + str(c)
         print(c)
-
-    print("Working")
     return a
 
 
@@ -39,20 +37,46 @@ def dictionaryAttack():
         words.append(a)
 
 def pickword():
-    print("Hello World")
+    b = random.randint(0,len(words))
+    c = words[b]
+
+    return c
 
 
+
+def salt(l):
+    if l == 1:
+        print("Hello World")
+    else:
+        print("Hello World")
 
 def mutatepass():
     print("Working")
 
-def genpass():
-    print("Working")
+
 
 def ranrule():
-    print("Hello World")
+
     for x in range(3):
         a = random.randint(1,len(rules))
+        if a == 1:
+            print("Rule 1")
+            rules.remove(1)
+            word = word + ruleone()
+
+        elif a == 2:
+            print("RUle 2")
+            word = word + pickword()
+            rules.remove(2)
+
+        else:
+            print("Rule 3")
+
+
+
+            rules.remove(3)
+
+
         print(a)
 
 
